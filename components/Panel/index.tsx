a6928ea2831ac83bbb5d826fe5f1c9a0a4e06301
pujiragil <ragilsg164@gmail.com>
2022-11-20 06:45:34 +0700

    create panel card components (mobile only)

--- components/Panel/index.tsx
+++ components/Panel/index.tsx
@@ -27,6 +27,70 @@ const Paragraph = styled(Subtitle)`
   font-weight: 300;
 `
 
+const PanelContent = styled(PanelHeader)`
+  text-align: left;
+  align-items: flex-start;
+`
+
+interface CardProps {
+  primary: boolean;
+}
+
+const Card = styled.div<CardProps>`
+  background: ${({ primary }) => primary ? "#E5EEFF" : "#FFF8E5"};
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  border-radius: 1rem;
+`
+
+const CardIcon = styled.img`
+  width: 80px;
+  height: 80px;
+  object-fit: cover;
+`
+
+const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+
+interface CardTitleProps {
+  primary: boolean;
+}
+
+const CardTitle = styled.h3<CardTitleProps>`
+  color: ${({ primary }) => primary ? "#5B93FF" : "#FAB400"};
+  font-size: 24px;
+  font-weight: 700;
+`
+
+const CardParagraph = styled(Paragraph)`
+  font-weight: 400;
+  color: #808093;
+`
+
+interface PanelItemProps {
+  primary: boolean;
+  imageLink: string;
+  title: string;
+}
+
+function PanelItem({ primary, imageLink , title }: PanelItemProps) {
+  return (
+    <Card primary={primary}>
+      <CardIcon src={imageLink} />
+
+      <CardContent>
+        <CardTitle primary={primary}>{title}</CardTitle>
+        <CardParagraph>Phasellus nec meligula. Cras vel intedum nisl. Inid ante orci.</CardParagraph>
+      </CardContent>
+    </Card>
+  )
+}
+
 export default function Panel() {
   return (
     <Container>
@@ -41,6 +105,12 @@ export default function Panel() {
           <SubHeading>We manage all those things by some different way</SubHeading>
           <Paragraph>Laboriosam aute dolor, culpa. Numquam aspernatur but ex so perspiciatis eaque sint. Architecto occaecat or veritatis but mollit so aut so irure. Do proident voluptas but non. Et consequuntur yet fugit est.</Paragraph>
         </PanelHeader>
+
+        <PanelContent>
+          <PanelItem primary={true} imageLink={"/panel-1.svg"} title={"Easy and quick start"} />
+          <PanelItem primary={false} imageLink={"/panel-2.svg"} title={"Wide rang of functions"} />
+          <PanelItem primary={true} imageLink={"/panel-3.svg"} title={"Customizable features"} />
+        </PanelContent>
       </PanelWrapper>
     </Container>
   )
